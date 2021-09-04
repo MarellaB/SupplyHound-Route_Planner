41db264aaf1c5616ee7d69f772bd21b606006973
MarellaB <brandonmarella@gmail.com>
2021-09-04 18:44:25 -0400

    Finished Task 2, added testing documentation to ReadMe, Added Comments

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # SupplyHound-Route_Planner
 Code Challenge for Supply Hound to design a route planner using The World Bank API.
+
+## To Use
+* Must Have NodeJS installed
+* Postman
+
+Create a new Postman (or equivilent API Test Application) GET request.
+
+Point the request to "http://localhsot:3000/" and use one of the following endpoints:
+* Task 1: "/getCountry"
+  * Query Parameters:
+    * countryCode = **###** (Replacing **###** with an appropriate Country Code)
+* Task 2: "/getNearbyCities"
+  * Query Parameters:
+    * minLongitude = **##** (replacing **##** with the longitude value)
+    * maxLongitude = **##** (replacing **##** with the longitude value)
+    * minLatitude = **##** (replacing **##** with the latitude value)
+    * maxLatitude = **##** (replacing **##** with the latitude value)
+* trask 3: [TODO]
--- index.js
+++ index.js
@@ -9,8 +9,9 @@ let responseObjectTemplate = {
   responseData: {}
 };
 
+// Find a single country based on a given Country Code
 app.get('/getCountry', async (req, res) => {
-  https.get(`https://api.worldbank.org/v2/country/${req.query.id}?format=json`, (resp) => {
+  https.get(`https://api.worldbank.org/v2/country/${req.query.countryCode}?format=json`, (resp) => {
     let responseData = '';
 
     // Whenever data is received, we store it in case it is returned in pieces and not a single unit.
@@ -27,7 +28,7 @@ app.get('/getCountry', async (req, res) => {
       if (!returnData[0].hasOwnProperty("message")) {
         responseObjectTemplate.responseType = "GOOD";
         responseObjectTemplate.responseData = {
-          name: returnData[1][0].name,
+          country: returnData[1][0].name,
           capital: returnData[1][0].capitalCity
         };
       } else {  // If a "message" property DOES exist, then we alert the user of a bad request and reason for failure.
@@ -39,6 +40,47 @@ app.get('/getCountry', async (req, res) => {
     });
   });  
 });
+
+// Find a list of cities based on a given latitude and longitude range
+app.get('/getNearbyCities', async (req, res) => {
+  const longitude = {
+    min: req.query.minLongitude,
+    max: req.query.maxLongitude
+  };
+  const latitude = {
+    min: req.query.minLatitude,
+    max: req.query.maxLatitude
+  };
+
+  https.get(`https://api.worldbank.org/v2/country/?format=json&per_page=500`, (resp) => {
+    let responseData = "";
+
+    resp.on("data", (chunk) => {
+      responseData += chunk;
+    });
+
+    resp.on('end', () => {
+      let nearbyCities = [];
+
+      JSON.parse(responseData)[1].forEach(city => {
+        if (city.longitude >= longitude.min && city.longitude <= longitude.max) {
+          if (city.latitude >= latitude.min && city.latitude <= latitude.max) {
+            nearbyCities.push(city.capitalCity);
+          }
+        }
+      });
+
+      responseObjectTemplate.responseType = "GOOD";
+      responseObjectTemplate.responseData = {
+        cities: nearbyCities,
+        longitude,
+        latitude
+      }
+
+      res.send(responseObjectTemplate);
+    })
+  });
+})
 
 
 // Tell the app to watch the given port
